c008085c667141e234b2a86d660bcc9996c3d4f4
Fedor Indutny <fedor@indutny.com>
2019-04-03 16:29:50 -0400

    options: add a bit of docs, more options

--- src/options.rs
+++ src/options.rs
@@ -1,8 +1,21 @@
 #[derive(Debug)]
 pub struct Options {
+    /// Default: 4kb (4096 bytes)
     pub block_size: usize,
+
+    /// Default: 1mb (1048576 bytes)
     pub buffer_size: usize,
+
+    /// Default: 4gb (4294967296 bytes)
+    pub max_file_size: usize,
+
+    /// Default: 100
+    pub max_open_files: usize,
+
+    /// Default: 8b (8 bytes)
     pub pad_size: usize,
+
+    /// Default: 0x3405_0d23_e85c_9e3a (a random value, really)
     pub magic: u64,
 }
 
@@ -11,6 +24,8 @@ impl Default for Options {
         Options {
             block_size: 4096,
             buffer_size: 1_048_576,
+            max_file_size: 4_294_967_296,
+            max_open_files: 100,
             pad_size: 8,
             magic: 0x3405_0d23_e85c_9e3au64,
         }
